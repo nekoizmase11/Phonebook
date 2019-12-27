237edf79b00735b987c94bd119f66d60096c82e6
nekoizmase11 <cusmirzver@gmail.com>
2019-12-27 21:39:01 +0100

    Models

---
+++ Phonebook/Phonebook.Bussines.Contracts/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook.Bussines.Contracts.Models
+{
+    public class Contact
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string LName { get; set; }
+        public string Email { get; set; }
+        public string Number { get; set; }
+        public int id_user { get; set; }
+
+        public Contact(int id, string name, string lName, string email, string number, int id_user)
+        {
+            this.id = id;
+            Name = name;
+            LName = lName;
+            Email = email;
+            Number = number;
+            this.id_user = id_user;
+        }
+    }
+}
---
+++ Phonebook/Phonebook.Bussines.Contracts/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook.Bussines.Contracts.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public User(int id, string email, string password)
+        {
+            this.id = id;
+            this.email = email;
+            this.password = password;
+        }
+    }
+}
