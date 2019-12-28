41ff7eab1e872fe2056bba21cde91f484170bca8
nekoizmase11 <cusmirzver@gmail.com>
2019-12-28 18:34:51 +0100

    SQLServer exceptions added

---
+++ Phonebook/Phonebook.Data/SQLServerExceptions/SQLServerDuplcateDataException.cs
@@ -0,0 +1,29 @@
+﻿using Phonebook.Bussines.Contracts.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook.Data.SQLServerExceptions
+{
+    public class SQLServerDuplcateDataException : DuplicateDataException
+    {
+        public SQLServerDuplcateDataException()
+        {
+        }
+
+        public SQLServerDuplcateDataException(string message) : base(message)
+        {
+        }
+
+        public SQLServerDuplcateDataException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected SQLServerDuplcateDataException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Phonebook/Phonebook.Data/SQLServerExceptions/SQLServerInvalidDataException.cs
@@ -0,0 +1,29 @@
+﻿using Phonebook.Bussines.Contracts.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook.Data.SQLServerExceptions
+{
+    public class SQLServerInvalidDataException : InvalidDataException
+    {
+        public SQLServerInvalidDataException()
+        {
+        }
+
+        public SQLServerInvalidDataException(string message) : base(message)
+        {
+        }
+
+        public SQLServerInvalidDataException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected SQLServerInvalidDataException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
