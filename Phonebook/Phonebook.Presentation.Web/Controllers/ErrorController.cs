a013f9664a0dd76fd10b9aaa4468e44f1a6b9700
nekoizmase11 <cusmirzver@gmail.com>
2019-12-30 11:45:10 +0100

    Error handled

---
+++ Phonebook/Phonebook.Presentation.Web/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Phonebook.Presentation.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        public ActionResult ServerError()
+        {
+            return View();
+        }
+    }
+}
--- Phonebook/Phonebook.Presentation.Web/Global.asax.cs
+++ Phonebook/Phonebook.Presentation.Web/Global.asax.cs
@@ -18,5 +18,42 @@ namespace Phonebook.Presentation.Web
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            Response.Clear();
+            // Response.Redirect("/Home/Error");
+
+            HttpException httpException = exception as HttpException;
+
+
+            if (httpException != null)
+            {
+                string action;
+
+                switch (httpException.GetHttpCode())
+                {
+                    case 404:
+                        // page not found
+                        action = "NotFound";
+                        break;
+                    case 500:
+                        // server error
+                        action = "ServerError";
+                        break;
+                    default:
+                        action = "Index";
+                        break;
+                }
+
+                // clear error on server
+                Server.ClearError();
+
+                //String.Format("/Home/{0}", action)
+                Response.Redirect(String.Format("/Error/{0}", action));
+            }
+
+        }
     }
 }
